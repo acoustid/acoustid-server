55b2a4350b3950515b63675a39304b7622987d17
Lukáš Lalinský <lalinsky@gmail.com>
2011-04-08 12:05:31 +0200

    Missing file

---
+++ acoustid/submission.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2011 Lukas Lalinsky
+# Distributed under the MIT license, see the LICENSE file for details. 
+
+import logging
+from sqlalchemy import sql
+from acoustid import tables as schema
+
+logger = logging.getLogger(__name__)
+
+
+def insert_submission(conn, data):
+    """
+    Insert a new submission into the database.
+    """
+    with conn.begin():
+        insert_stmt = schema.submission.insert().values({
+            'fingerprint': data['fingerprint'],
+            'length': data['length'],
+            'bitrate': data.get('bitrate'),
+            'mbid': data.get('mbid'),
+            'puid': data.get('puid'),
+            'source_id': 1,
+        })
+        id = conn.execute(insert_stmt).inserted_primary_key[0]
+    logger.info("Inserted submission %r with data %r", id, data)
+
