f1e41208beaca04187362bf719ca24cf39998acc
Lukáš Lalinský <lalinsky@gmail.com>
2011-04-11 14:28:01 +0200

    Add an option to run scripts in quiet mode

--- acoustid/script.py
+++ acoustid/script.py
@@ -26,21 +26,26 @@ class Script(object):
             handler.setFormatter(logging.Formatter('acoustid[%(process)s]: %(name)s: %(message)s'))
             logging.getLogger().addHandler(handler)
 
-    def setup_console_logging(self):
+    def setup_console_logging(self, quiet=False):
         handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(name)s - %(message)s', '%H:%M:%S'))
+        if quiet:
+            handler.setLevel(logging.ERROR)
         logging.getLogger().addHandler(handler)
+
 
 
 def run_script(func):
     parser = OptionParser()
     parser.add_option("-c", "--config", dest="config",
         help="configuration file", metavar="FILE")
+    parser.add_option("-q", "--quiet", dest="quiet", action="store_true",
+        default=False, help="don't print info messages to stdout")
     (options, args) = parser.parse_args()
     if not options.config:
         parser.error('no configuration file')
     script = Script(options.config)
-    script.setup_console_logging()
+    script.setup_console_logging(options.quiet)
     logger.info("Running script %s", sys.argv[0])
     try:
         func(script, options, args)
