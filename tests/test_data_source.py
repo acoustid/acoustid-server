ab78ba1d58318102f772c4af6bff9fc2db89d705
Lukáš Lalinský <lalinsky@gmail.com>
2011-04-21 12:52:14 +0200

    Tests for acoustid.data.source

---
+++ tests/test_data_source.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2011 Lukas Lalinsky
+# Distributed under the MIT license, see the LICENSE file for details.
+
+from nose.tools import *
+from tests import prepare_database, with_database
+from acoustid.data.source import find_or_insert_source
+
+
+@with_database
+def test_find_or_insert_source(conn):
+    rows = conn.execute("SELECT id, account_id, application_id FROM source ORDER BY id").fetchall()
+    expected_rows = [
+        (1, 1, 1),
+        (2, 2, 2),
+    ]
+    assert_equals(expected_rows, rows)
+    id = find_or_insert_source(conn, 1, 1)
+    assert_equals(1, id)
+    id = find_or_insert_source(conn, 2, 2)
+    assert_equals(2, id)
+    id = find_or_insert_source(conn, 1, 2)
+    assert_equals(3, id)
+    rows = conn.execute("SELECT id, account_id, application_id FROM source ORDER BY id").fetchall()
+    expected_rows = [
+        (1, 1, 1),
+        (2, 2, 2),
+        (3, 1, 2),
+    ]
+    assert_equals(expected_rows, rows)
+
