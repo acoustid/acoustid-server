495a282ac443d608e60109148fa8579338509371
Lukáš Lalinský <lalinsky@gmail.com>
2011-04-26 14:08:43 +0000

    Correctly set the syslog facility

--- acoustid/utils.py
+++ acoustid/utils.py
@@ -45,7 +45,26 @@ class LocalSysLogHandler(Handler):
     Logging handler that logs to the local syslog using the syslog module
     """
 
-    facility_names = SysLogHandler.facility_names
+    facility_names = {
+        "auth":     syslog.LOG_AUTH,
+        "cron":     syslog.LOG_CRON,
+        "daemon":   syslog.LOG_DAEMON,
+        "kern":     syslog.LOG_KERN,
+        "lpr":      syslog.LOG_LPR,
+        "mail":     syslog.LOG_MAIL,
+        "news":     syslog.LOG_NEWS,
+        "syslog":   syslog.LOG_SYSLOG,
+        "user":     syslog.LOG_USER,
+        "uucp":     syslog.LOG_UUCP,
+        "local0":   syslog.LOG_LOCAL0,
+        "local1":   syslog.LOG_LOCAL1,
+        "local2":   syslog.LOG_LOCAL2,
+        "local3":   syslog.LOG_LOCAL3,
+        "local4":   syslog.LOG_LOCAL4,
+        "local5":   syslog.LOG_LOCAL5,
+        "local6":   syslog.LOG_LOCAL6,
+        "local7":   syslog.LOG_LOCAL7,
+    }
 
     priority_map = {
         "DEBUG": syslog.LOG_DEBUG,
@@ -57,12 +76,13 @@ class LocalSysLogHandler(Handler):
 
     def __init__(self, ident=None, facility=syslog.LOG_USER, log_pid=False):
         Handler.__init__(self)
+        self.facility = facility
         if isinstance(facility, basestring):
-            facility = self.facility_names[facility]
+            self.facility = self.facility_names[facility]
         options = 0
         if log_pid:
             options |= syslog.LOG_PID
-        syslog.openlog(ident, options, facility)
+        syslog.openlog(ident, options, self.facility)
         self.formatter = None
 
     def close(self):
@@ -75,7 +95,7 @@ class LocalSysLogHandler(Handler):
             if isinstance(msg, unicode):
                 msg = msg.encode('utf-8')
             priority = self.priority_map[record.levelname]
-            syslog.syslog(priority, msg)
+            syslog.syslog(self.facility | priority, msg)
         except StandardError:
             self.handleError(record)
 
