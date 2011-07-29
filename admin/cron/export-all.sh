964c09e4b3923439d7208defe321934ec0b49f4e
Lukáš Lalinský <lalinsky@gmail.com>
2011-07-29 13:00:13 +0200

    Updated script to export the database

--- admin/cron/export-all.sh
+++ admin/cron/export-all.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+DIR=/home/acoustid/acoustid-server/
+
 TARGET_DIR=/home/acoustid/data/`date '+%Y-%m-%d'`/
 
-cd /home/acoustid/acoustid-server/
-rm -rf /tmp/acoustid-dump/ && python admin/export-tables.py conf/acoustid.xml
+cd $DIR
+rm -rf /tmp/acoustid-dump/ && PYTHONPATH=$DIR python scripts/export_tables.py -c acoustid.conf
 cd /tmp
 tar -jcvf acoustid-dump.tar.bz2 acoustid-dump
-mkdir $TARGET_DIR
+mkdir -p $TARGET_DIR
 mv acoustid-dump.tar.bz2 $TARGET_DIR
 
