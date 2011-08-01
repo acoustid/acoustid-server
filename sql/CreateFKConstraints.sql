58c254f4f033e4f0b59c81a1dc369fae5772018d
Lukáš Lalinský <lalinsky@gmail.com>
2011-08-01 10:47:03 +0200

    FK constraints for fingerprint_source

--- sql/CreateFKConstraints.sql
+++ sql/CreateFKConstraints.sql
@@ -10,14 +10,6 @@ ALTER TABLE source ADD CONSTRAINT source_fk_account_id
     FOREIGN KEY (account_id)
     REFERENCES account (id);
 
-ALTER TABLE fingerprint ADD CONSTRAINT fingerprint_fk_source_id
-    FOREIGN KEY (source_id)
-    REFERENCES source (id);
-
-ALTER TABLE fingerprint ADD CONSTRAINT fingerprint_fk_submission_id
-    FOREIGN KEY (submission_id)
-    REFERENCES submission (id);
-
 ALTER TABLE fingerprint ADD CONSTRAINT fingerprint_fk_format_id
     FOREIGN KEY (format_id)
     REFERENCES format (id);
@@ -25,6 +17,18 @@ ALTER TABLE fingerprint ADD CONSTRAINT fingerprint_fk_format_id
 ALTER TABLE fingerprint ADD CONSTRAINT fingerprint_fk_track_id
     FOREIGN KEY (track_id)
     REFERENCES track (id);
+
+ALTER TABLE fingerprint_source ADD CONSTRAINT fingerprint_source_fk_fingerprint_id
+    FOREIGN KEY (fingerprint_id)
+    REFERENCES fingerprint (id);
+
+ALTER TABLE fingerprint_source ADD CONSTRAINT fingerprint_source_fk_submission_id
+    FOREIGN KEY (submission_id)
+    REFERENCES submission (id);
+
+ALTER TABLE fingerprint_source ADD CONSTRAINT fingerprint_source_fk_source_id
+    FOREIGN KEY (source_id)
+    REFERENCES source (id);
 
 ALTER TABLE track ADD CONSTRAINT track_fk_new_id
     FOREIGN KEY (new_id)
