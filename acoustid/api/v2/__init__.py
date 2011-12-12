d351be6a08fd4d3ab9ab49bd147ab6af30773baf
Lukáš Lalinský <lalinsky@gmail.com>
2011-12-12 13:24:19 +0000

    Return release group title

--- acoustid/api/v2/__init__.py
+++ acoustid/api/v2/__init__.py
@@ -240,6 +240,8 @@ class LookupHandler(APIHandler):
         release_group = {'id': m['release_group_id']}
         if only_id:
             return release_group
+        release_group['title'] = m['release_group_title'] or ''
+        if m['release_group_type']:
             release_group['type'] = m['release_group_type']
         if m['release_group_artists']:
             release_group['artists'] = m['release_group_artists']
