930f5364b3b511fd18c49ae549a0cddd85d87398
Lukáš Lalinský <lalinsky@gmail.com>
2012-01-14 13:18:11 +0100

    Use the max_document_id attribute in the index itself

--- scripts/update_index.py
+++ scripts/update_index.py
@@ -9,27 +9,24 @@ from acoustid.script import run_script
 
 
 def main(script, opts, args):
-    conn = script.engine.connect()
-    with conn.begin():
-        rows = conn.execute("""
-            SELECT id, acoustid_extract_query(fingerprint) AS fingerprint
-            FROM fingerprint
-            WHERE id > (SELECT fingerprint_id FROM fingerprint_index_queue)
-            ORDER BY id
-            LIMIT 10000
-        """)
-        max_id = 0
-        for row in rows:
-            if not max_id:
-                idx = IndexClient(host=script.config.index.host,
-                                  port=script.config.index.port)
-                idx.begin()
-            max_id = row['id']
-            idx.insert(row['id'], row['fingerprint'])
-        if max_id:
-            idx.commit()
-            idx.close()
-            conn.execute("UPDATE fingerprint_index_queue SET fingerprint_id = %s", (max_id,))
+    db = script.engine.connect()
+    idx = script.index.connect()
+    max_id = int(idx.get_attribute('max_document_id') or '0')
+    rows = db.execute("""
+        SELECT id, acoustid_extract_query(fingerprint) AS fingerprint
+        FROM fingerprint WHERE id > %s ORDER BY id LIMIT 10000
+    """, max_id)
+    have_rows = False
+    for row in rows:
+        if not have_rows:
+            idx.begin()
+            have_rows = True
+        idx.insert(row['id'], row['fingerprint'])
+    if have_rows:
+        idx.commit()
+    idx.close()
+    db.close()
+
 
 run_script(main)
 
