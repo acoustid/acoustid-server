9d004e8ca08aad82e10b8b1574ffb36016a9b545
Lukáš Lalinský <lalinsky@gmail.com>
2012-10-19 10:34:36 +0000

    Add update_lookup_stats.py to the hourly cron task

--- admin/cron/hourly.sh
+++ admin/cron/hourly.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
+LOCKNAME=acoustid-hourly
+. `dirname $0`/lock.sh
+
+DIR=`dirname $0`/../..
+export PYTHONPATH=$DIR
+
+if [ "$1" != "--slave" ]; then
+	python $DIR/scripts/update_lookup_stats.py -q -c $DIR/acoustid.conf
+fi
+
