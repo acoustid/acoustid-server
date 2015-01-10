b45669de7af7cbb024c7324cc402f88c5f322379
Lukáš Lalinský <lukas@oxygene.sk>
2015-01-10 12:13:03 +0100

    Use virtualenv for wsgi app

--- acoustid/wsgi.py
+++ acoustid/wsgi.py
@@ -5,8 +5,15 @@
 # uwsgi -w acoustid.wsgi --pythonpath ~/acoustid/ --env ACOUSTID_CONFIG=~/acoustid/acoustid.conf --http :9090
 # uwsgi -w acoustid.wsgi --pythonpath ~/acoustid/ --env ACOUSTID_CONFIG=~/acoustid/acoustid.conf -M -L --socket 127.0.0.1:1717
 
-import os
+import os, sys
+
+base_dir = os.path.join(os.path.dirname(__file__), '..')
+
+activate_this = os.path.join(base_dir, 'e', 'bin', 'activate_this.py')
+if os.path.exists(activate_this):
+    execfile(activate_this, dict(__file__=activate_this))
+
+sys.path.insert(0, base_dir)
+
 from acoustid.server import make_application
-
 application = make_application(os.environ['ACOUSTID_CONFIG'])
-
