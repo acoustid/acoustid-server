80f4185c70e78c8381bd6db3ab2751c4b761b747
Lukáš Lalinský <lalinsky@gmail.com>
2019-04-11 15:51:54 +0200

    Don't run import in quiet mode

--- admin/run-import.sh
+++ admin/run-import.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="/opt/acoustid/server"
-exec /opt/acoustid/server/venv/bin/python /opt/acoustid/server/scripts/import_submissions.py -q -c "$ACOUSTID_CONFIG"
+exec /opt/acoustid/server/venv/bin/python /opt/acoustid/server/scripts/import_submissions.py -c "$ACOUSTID_CONFIG"
