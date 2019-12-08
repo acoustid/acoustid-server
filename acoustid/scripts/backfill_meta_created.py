7769bf8447fec1702efb7ae8a76f7570fcef6c72
Lukáš Lalinský <lalinsky@gmail.com>
2019-12-08 08:25:53 +0100

    Stop backfill_meta_created

--- acoustid/scripts/backfill_meta_created.py
+++ acoustid/scripts/backfill_meta_created.py
@@ -26,12 +26,19 @@ def run_backfill_meta_created(script, opts, args):
         WHERE meta.id = meta_created.meta_id AND meta.created IS NULL AND meta.id >= %(first_meta_id)s AND meta.id < %(last_meta_id)s
     """
 
+    with script.context() as ctx:
+        first_meta_id = int(ctx.redis.get('backfill_meta_created_first_id') or 0)
+        fingerprint_db = ctx.db.get_fingerprint_db()
+        max_meta_id = fingerprint_db.execute("GET max(id) FROM meta").scalar()
+        if first_meta_id > max_meta_id:
+            return
+
     for i in range(100):
         with script.context() as ctx:
-            first_meta_id = int(ctx.redis.get('backfill_meta_created_first_id') or 0)
             last_meta_id = first_meta_id + 10000
             fingerprint_db = ctx.db.get_fingerprint_db()
             result = fingerprint_db.execute(update_query, {'first_meta_id': first_meta_id, 'last_meta_id': last_meta_id})
             logger.info('Added create date to %s meta entries between (%d and %d)', result.rowcount, first_meta_id, last_meta_id)
             ctx.db.session.commit()
             ctx.redis.set('backfill_meta_created_first_id', str(last_meta_id))
+            first_meta_id = last_meta_id
