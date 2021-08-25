8c5d7137b2adb734bfac1e8d4fc68692b22eddb2
Lukáš Lalinský <lalinsky@gmail.com>
2021-08-25 12:05:51 +0200

    Do not sort when importing submissions

--- acoustid/data/submission.py
+++ acoustid/data/submission.py
@@ -195,8 +195,6 @@ def import_queued_submissions(ingest_db, app_db, fingerprint_db, index, limit=10
     query = (
         schema.submission.select(schema.submission.c.handled.is_(False))
         .with_for_update(skip_locked=True)
-        .where(schema.submission.c.account_id.is_(None) | (schema.submission.c.account_id != 1345917))
-        .order_by(schema.submission.c.id.desc())
     )
     if ids is not None:
         query = query.where(schema.submission.c.id.in_(ids))
