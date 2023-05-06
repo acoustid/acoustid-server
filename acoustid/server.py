c08bd8a34d7478b6e0258470e35e392da9133b77
Lukáš Lalinský <lalinsky@gmail.com>
2023-05-06 20:37:32 +0200

    Fix werkzeug types

--- acoustid/server.py
+++ acoustid/server.py
@@ -24,37 +24,57 @@ if TYPE_CHECKING:
     from wsgiref.types import WSGIApplication, WSGIEnvironment, StartResponse
 
 
+endpoints = {
+    'health': acoustid.api.HealthHandler,
+    'health_ro': acoustid.api.ReadOnlyHealthHandler,
+    'health_docker': acoustid.api.ReadOnlyHealthHandler,
+    'v1.lookup': acoustid.api.v1.LookupHandler,
+    'v1.submit': acoustid.api.v1.SubmitHandler,
+    'v2.lookup': acoustid.api.v2.LookupHandler,
+    'v2.submit': acoustid.api.v2.SubmitHandler,
+    'v2.submission_status': acoustid.api.v2.SubmissionStatusHandler,
+    'v2.fingerprint': acoustid.api.v2.GetFingerprintHandler,
+    'v2.track.list_by_mbid': acoustid.api.v2.TrackListByMBIDHandler,
+    'v2.track.list_by_puid': acoustid.api.v2.TrackListByPUIDHandler,
+    'v2.user.lookup': acoustid.api.v2.UserLookupHandler,
+    'v2.user.create_anonymous': acoustid.api.v2.UserCreateAnonymousHandler,
+    'v2.user.create_musicbrainz': acoustid.api.v2.UserCreateMusicBrainzHandler,
+    'v2.internal.update_lookup_stats': acoustid.api.v2.internal.UpdateLookupStatsHandler,
+    'v2.internal.update_user_agent_stats': acoustid.api.v2.internal.UpdateUserAgentStatsHandler,
+    'v2.internal.lookup_stats': acoustid.api.v2.internal.LookupStatsHandler,
+    'v2.internal.create_account': acoustid.api.v2.internal.CreateAccountHandler,
+    'v2.internal.create_application': acoustid.api.v2.internal.CreateApplicationHandler,
+    'v2.internal.update_application_status': acoustid.api.v2.internal.UpdateApplicationStatusHandler,
+    'v2.internal.check_application': acoustid.api.v2.internal.CheckApplicationHandler,
+}
+
+
 api_url_rules = [
-    Rule('/_health', endpoint=acoustid.api.HealthHandler),
-    Rule('/_health_ro', endpoint=acoustid.api.ReadOnlyHealthHandler),
-    Rule('/_health_docker', endpoint=acoustid.api.ReadOnlyHealthHandler),
-    Rule('/lookup', endpoint=acoustid.api.v1.LookupHandler),
-    Rule('/submit', endpoint=acoustid.api.v1.SubmitHandler),
+    Rule('/_health', endpoint='health'),
+    Rule('/_health_ro', endpoint='health_ro'),
+    Rule('/_health_docker', endpoint='header_docker'),
+    Rule('/lookup', endpoint='v1.lookup'),
+    Rule('/submit', endpoint='v1.submit'),
     Submount('/v2', [
-        Rule('/lookup', endpoint=acoustid.api.v2.LookupHandler),
-        Rule('/submit', endpoint=acoustid.api.v2.SubmitHandler),
-        Rule('/submission_status', endpoint=acoustid.api.v2.SubmissionStatusHandler),
-        Rule('/fingerprint', endpoint=acoustid.api.v2.misc.GetFingerprintHandler),
-        Rule('/track/list_by_mbid', endpoint=acoustid.api.v2.misc.TrackListByMBIDHandler),
-        Rule('/track/list_by_puid', endpoint=acoustid.api.v2.misc.TrackListByPUIDHandler),
-        Rule('/user/lookup', endpoint=acoustid.api.v2.misc.UserLookupHandler),
-        Rule('/user/create_anonymous', endpoint=acoustid.api.v2.misc.UserCreateAnonymousHandler),
-        Rule('/user/create_musicbrainz', endpoint=acoustid.api.v2.misc.UserCreateMusicBrainzHandler),
+        Rule('/lookup', endpoint='v2.lookup'),
+        Rule('/submit', endpoint='v2.submit'),
+        Rule('/submission_status', endpoint='v2.submission_status'),
+        Rule('/fingerprint', endpoint='v2.fingerprint'),
+        Rule('/track/list_by_mbid', endpoint='v2.track.list_by_mbid'),
+        Rule('/track/list_by_puid', endpoint='v2.track.list_by_puid'),
+        Rule('/user/lookup', endpoint='v2.user.lookup'),
+        Rule('/user/create_anonymous', endpoint='v2.user.create_anonymous'),
+        Rule('/user/create_musicbrainz', endpoint='v2.user.create_musicbrainz'),
         Submount('/internal', [
-            Rule('/update_lookup_stats', endpoint=acoustid.api.v2.internal.UpdateLookupStatsHandler),
-            Rule('/update_user_agent_stats', endpoint=acoustid.api.v2.internal.UpdateUserAgentStatsHandler),
-            Rule('/lookup_stats', endpoint=acoustid.api.v2.internal.LookupStatsHandler),
-            Rule('/create_account', endpoint=acoustid.api.v2.internal.CreateAccountHandler),
-            Rule('/create_application', endpoint=acoustid.api.v2.internal.CreateApplicationHandler),
-            Rule('/update_application_status', endpoint=acoustid.api.v2.internal.UpdateApplicationStatusHandler),
-            Rule('/check_application', endpoint=acoustid.api.v2.internal.CheckApplicationHandler),
+            Rule('/update_lookup_stats', endpoint='v2.internal.update_lookup_stats'),
+            Rule('/update_user_agent_stats', endpoint='v2.internal.update_user_agent_stats'),
+            Rule('/lookup_stats', endpoint='v2.internal.lookup_stats'),
+            Rule('/create_account', endpoint='v2.internal.create_account'),
+            Rule('/create_application', endpoint='v2.internal.create_application'),
+            Rule('/update_application_status', endpoint='v2.internal.update_application_status'),
+            Rule('/check_application', endpoint='v2.internal.check_application'),
         ]),
     ]),
-]  # type: List[RuleFactory]
-
-admin_url_rules = [
-    Submount('/admin', [
-    ])
 ]  # type: List[RuleFactory]
 
 
@@ -65,34 +85,33 @@ MAX_FORM_MEMORY_SIZE = 100 * 1024
 
 class Server(Script):
 
-    def __init__(self, config_path):
-        # type: (str) -> None
+    def __init__(self, config_path: str) -> None:
         super(Server, self).__init__(config_path)
-        url_rules = api_url_rules + admin_url_rules
+        url_rules = api_url_rules
         self.url_map = Map(url_rules, strict_slashes=False)
 
-    def __call__(self, environ, start_response):
-        # type: (WSGIEnvironment, StartResponse) -> Iterable[bytes]
+    def __call__(self, environ: WSGIEnvironment, start_response: StartResponse) -> Iterable[bytes]:
         return self.wsgi_app(environ, start_response)
 
-    def wsgi_app(self, environ, start_response):
-        # type: (WSGIEnvironment, StartResponse) -> Iterable[bytes]
+    def wsgi_app(self, environ: WSGIEnvironment, start_response: StartResponse) -> Iterable[bytes]:
         urls = self.url_map.bind_to_environ(environ)
         try:
-            handler_class, args = urls.match()
+            endpoint, args = urls.match()
+            handler_class = endpoints.get(endpoint)
+            if handler_class is None:
+                raise BadRequest('Unknown endpoint')
             with self.context() as ctx:
                 handler = handler_class(ctx, **args)
                 request = Request(environ)
                 request.max_content_length = MAX_CONTENT_LENGTH
-                request.max_form_parts = MAX_FORM_PARTS  # type: ignore
+                request.max_form_parts = MAX_FORM_PARTS
                 request.max_form_memory_size = MAX_FORM_MEMORY_SIZE
                 response = handler.handle(request)
         except HTTPException as e:
             return e(environ, start_response)
         return response(environ, start_response)
 
-    def setup_sentry(self):
-        # type: () -> None
+    def setup_sentry(self) -> None:
         sentry_sdk.init(self.config.sentry.api_dsn, release=GIT_RELEASE)
 
 
