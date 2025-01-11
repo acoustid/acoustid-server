e983f3eca672372010b48c1c13e3e80f3f0d4b84
Lukas Lalinsky <llalinsky@live365.com>
2025-01-11 09:43:48 +0100

    Ignore submissions without any public metadata

--- acoustid/api/v2/__init__.py
+++ acoustid/api/v2/__init__.py
@@ -990,6 +990,8 @@ class SubmitHandler(APIHandler):
                     values["meta"] = meta_values
                 if p["foreignid"]:
                     values["foreignid"] = p["foreignid"]
+                if values.get("mbid") is None and values.get("puid") is None and values.get("meta") is None:
+                    continue
                 id = insert_submission(ingest_db, values)
                 ids.add(id)
                 submission = {"id": id, "status": "pending"}
