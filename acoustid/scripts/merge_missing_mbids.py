ec697c1201321c4e37dfece4f91d891785fb961a
Lukas Lalinsky <llalinsky@live365.com>
2025-02-21 16:46:19 +0100

    Add locking

--- acoustid/scripts/merge_missing_mbids.py
+++ acoustid/scripts/merge_missing_mbids.py
@@ -4,12 +4,24 @@
 # Distributed under the MIT license, see the LICENSE file for details.
 
 import logging
+import zlib
 from contextlib import ExitStack
+
+import sqlalchemy as sa
 
 from acoustid.data.track import merge_missing_mbid
 from acoustid.script import Script
 
 logger = logging.getLogger(__name__)
+
+
+def try_lock(db: sa.engine.Connection, name: str, params: str) -> bool:
+    lock_id1 = zlib.crc32(name.encode())
+    lock_id2 = zlib.crc32(params.encode())
+    return db.execute(
+        sa.text("SELECT pg_try_advisory_xact_lock(:lock_id1, :lock_id2)"),
+        {"lock_id1": lock_id1, "lock_id2": lock_id2},
+    ).scalar()
 
 
 def run_merge_missing_mbid(script: Script, mbid: str):
@@ -19,8 +31,13 @@ def run_merge_missing_mbid(script: Script, mbid: str):
 
     with ExitStack() as stack:
         fingerprint_db = stack.enter_context(script.db_engines["fingerprint"].begin())
+        if not try_lock(fingerprint_db, "merge_missing_mbid", mbid):
+            logger.info("MBID %s is already being merged", mbid)
+            return
+
         ingest_db = stack.enter_context(script.db_engines["ingest"].begin())
         musicbrainz_db = stack.enter_context(script.db_engines["musicbrainz"].begin())
+
         merge_missing_mbid(
             fingerprint_db=fingerprint_db,
             ingest_db=ingest_db,
