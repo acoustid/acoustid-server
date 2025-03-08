88c80178a8fc5b3b0be1f48b4d933c5dc4aedc9d
Lukas Lalinsky <llalinsky@live365.com>
2025-03-08 18:17:38 +0100

    Turn config_file into a pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
+import os
+
 import pytest
 
 import tests
 
 
+@pytest.fixture(scope="session")
+def config_file() -> str:
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..", "acoustid-test.conf"
+    )
+
+
 @pytest.fixture(scope="session", autouse=True)
-def setup():
-    tests.setup()
+def setup(config_file: str):
+    tests.setup(config_file)
     yield
     tests.teardown()
