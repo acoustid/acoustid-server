8a6a3176fc84dd1147e7a6dc630746f8cbb312b0
Lukas Lalinsky <llalinsky@live365.com>
2025-03-11 08:59:46 +0100

    More changes

--- acoustid/data/account.py
+++ acoustid/data/account.py
@@ -2,7 +2,7 @@
 # Distributed under the MIT license, see the LICENSE file for details.
 
 import logging
-from typing import Any, Dict, Optional, Tuple
+from typing import Any
 
 from sqlalchemy import sql
 
@@ -13,57 +13,51 @@ from acoustid.utils import generate_api_key
 logger = logging.getLogger(__name__)
 
 
-def lookup_account_id_by_name(conn: AppDB, name: str) -> int:
-    query = sql.select([schema.account.c.id], schema.account.c.name == name)
+def lookup_account_id_by_name(conn: AppDB, name: str) -> int | None:
+    query = sql.select(schema.account.c.id).where(schema.account.c.name == name)
     return conn.execute(query).scalar()
 
 
-def lookup_account_id_by_apikey(conn, apikey):
-    # type: (AppDB, str) -> int
-    query = sql.select([schema.account.c.id], schema.account.c.apikey == apikey)
+def lookup_account_id_by_apikey(conn: AppDB, apikey: str) -> int | None:
+    query = sql.select(schema.account.c.id).where(schema.account.c.apikey == apikey)
     return conn.execute(query).scalar()
 
 
-def lookup_account_id_by_mbuser(conn, mbuser):
-    # type: (AppDB, str) -> int
-    query = sql.select(
-        [schema.account.c.id],
-        sql.func.lower(schema.account.c.mbuser) == sql.func.lower(mbuser),
-    )
-    return conn.execute(query).scalar()
-
-
-def lookup_account_id_by_openid(conn, openid):
-    # type: (AppDB, str) -> int
-    query = sql.select(
-        [schema.account_openid.c.account_id], schema.account_openid.c.openid == openid
-    )
-    return conn.execute(query).scalar()
-
-
-def get_account_details(conn, id):
-    # type: (AppDB, int) -> Optional[Dict[Any, Any]]
-    query = schema.account.select(schema.account.c.id == id)
-    return conn.execute(query).fetchone()
-
-
-def get_account_details_by_mbuser(conn, mbuser):
-    # type: (AppDB, str) -> Optional[Dict[Any, Any]]
-    query = schema.account.select(
+def lookup_account_id_by_mbuser(conn: AppDB, mbuser: str) -> int | None:
+    query = sql.select(schema.account.c.id).where(
         sql.func.lower(schema.account.c.mbuser) == sql.func.lower(mbuser)
     )
-    return conn.execute(query).fetchone()
+    return conn.execute(query).scalar()
 
 
-def update_account_lastlogin(conn, id):
-    # type: (AppDB, int) -> None
+def lookup_account_id_by_openid(conn: AppDB, openid: str) -> int | None:
+    query = sql.select(schema.account_openid.c.account_id).where(
+        schema.account_openid.c.openid == openid
+    )
+    return conn.execute(query).scalar()
+
+
+def get_account_details(conn: AppDB, id: int) -> dict[str, Any] | None:
+    query = sql.select(schema.account).where(schema.account.c.id == id)
+    row = conn.execute(query).fetchone()
+    return dict(row) if row else None
+
+
+def get_account_details_by_mbuser(conn: AppDB, mbuser: str) -> dict[dict, Any] | None:
+    query = sql.select(schema.account).where(
+        sql.func.lower(schema.account.c.mbuser) == sql.func.lower(mbuser)
+    )
+    row = conn.execute(query).fetchone()
+    return dict(row) if row else None
+
+
+def update_account_lastlogin(conn: AppDB, id: int) -> None:
     update_stmt = schema.account.update().where(schema.account.c.id == id)
     update_stmt = update_stmt.values(lastlogin=sql.text("now()"))
     conn.execute(update_stmt)
 
 
-def insert_account(conn, data):
-    # type: (AppDB, Dict[str, Any]) -> Tuple[int, str]
+def insert_account(conn: AppDB, data: dict[str, Any]) -> tuple[int, str]:
     """
     Insert a new account into the database
     """
@@ -82,28 +76,29 @@ def insert_account(conn, data):
         )
         .returning(schema.account.c.id, schema.account.c.apikey)
     )
-    id, api_key = conn.execute(insert_stmt).fetchone()
+    row = conn.execute(insert_stmt).fetchone()
+    if row is None:
+        raise Exception("Failed to insert account")
+
     if "openid" in data:
-        insert_stmt = schema.account_openid.insert().values(
+        insert_openid_stmt = schema.account_openid.insert().values(
             {
-                "account_id": id,
+                "account_id": row.id,
                 "openid": data["openid"],
             }
         )
-        conn.execute(insert_stmt)
-    logger.debug("Inserted account %r with data %r", id, data)
-    return id, api_key
+        conn.execute(insert_openid_stmt)
+    logger.debug("Inserted account %r with data %r", row.id, data)
+    return row.id, row.api_key
 
 
-def reset_account_apikey(conn, id):
-    # type: (AppDB, int) -> None
+def reset_account_apikey(conn: AppDB, id: int) -> None:
     update_stmt = schema.account.update().where(schema.account.c.id == id)
     update_stmt = update_stmt.values(apikey=generate_api_key())
     conn.execute(update_stmt)
     logger.debug("Reset API key for account %r", id)
 
 
-def is_moderator(conn, id):
-    # type: (AppDB, int) -> bool
-    query = sql.select([schema.account.c.mbuser], schema.account.c.id == id)
+def is_moderator(conn: AppDB, id: int) -> bool:
+    query = sql.select(schema.account.c.mbuser).where(schema.account.c.id == id)
     return bool(conn.execute(query).scalar())
--- acoustid/data/application.py
+++ acoustid/data/application.py
@@ -2,7 +2,7 @@
 # Distributed under the MIT license, see the LICENSE file for details.
 
 import logging
-from typing import Any, Dict, List, Tuple
+from typing import Any
 
 from sqlalchemy import sql
 
@@ -13,39 +13,46 @@ from acoustid.utils import generate_api_key
 logger = logging.getLogger(__name__)
 
 
-def lookup_application_id_by_apikey(conn, apikey, only_active=False):
-    # type: (AppDB, str, bool) -> int
-    query = sql.select([schema.application.c.id], schema.application.c.apikey == apikey)
+def lookup_application_id_by_apikey(
+    conn: AppDB, apikey: str, only_active: bool = False
+) -> int | None:
+    query = sql.select(schema.application.c.id).where(
+        schema.application.c.apikey == apikey
+    )
     if only_active:
         query = query.where(schema.application.c.active.is_(True))
     return conn.execute(query).scalar()
 
 
-def lookup_application_id(conn, application_id, account_id=None):
-    # type: (AppDB, str, int | None) -> int
-    query = sql.select(
-        [schema.application.c.id], schema.application.c.id == application_id
+def lookup_application_id(
+    conn: AppDB, application_id: str, account_id: int | None = None
+) -> int | None:
+    query = sql.select(schema.application.c.id).where(
+        schema.application.c.id == application_id
     )
     if account_id is not None:
         query = query.where(schema.application.c.account_id == account_id)
     return conn.execute(query).scalar()
 
 
-def find_applications_by_account(conn, account_id):
-    # type: (AppDB, int) -> List[Dict[Any, Any]]
-    query = schema.application.select(schema.application.c.account_id == account_id)
+def find_applications_by_account(conn: AppDB, account_id: int) -> list[dict[str, Any]]:
+    query = sql.select(schema.application).where(
+        schema.application.c.account_id == account_id
+    )
     query = query.order_by(schema.application.c.name)
     return [dict(i) for i in conn.execute(query).fetchall()]
 
 
-def find_applications_by_apikeys(conn, apikeys):
-    # type: (AppDB, List[str]) -> List[Dict[Any, Any]]
-    query = schema.application.select(schema.application.c.apikey.in_(apikeys))
+def find_applications_by_apikeys(
+    conn: AppDB, apikeys: list[str]
+) -> list[dict[str, Any]]:
+    query = sql.select(schema.application).where(
+        schema.application.c.apikey.in_(apikeys)
+    )
     return [dict(i) for i in conn.execute(query).fetchall()]
 
 
-def insert_application(conn, data):
-    # type: (AppDB, Dict[str, Any]) -> Tuple[int, str]
+def insert_application(conn: AppDB, data: dict[str, Any]) -> tuple[int, str]:
     """
     Insert a new application into the database
     """
@@ -65,8 +72,7 @@ def insert_application(conn, data):
     return id, api_key
 
 
-def update_application(conn, id, data):
-    # type: (AppDB, int, Dict[str, Any]) -> int
+def update_application(conn: AppDB, id: int, data: dict[str, Any]) -> int:
     update_stmt = schema.application.update().where(schema.application.c.id == id)
     update_stmt = update_stmt.values(
         {
@@ -81,8 +87,7 @@ def update_application(conn, id, data):
     return id
 
 
-def update_application_status(conn, id, active):
-    # type: (AppDB, int, bool) -> int
+def update_application_status(conn: AppDB, id: int, active: bool) -> int:
     data = {"active": active}
     update_stmt = schema.application.update().where(schema.application.c.id == id)
     update_stmt = update_stmt.values(data)
