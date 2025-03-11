6d1c919ea9d6d7a70684417b45978ea2c9cd042d
Lukas Lalinsky <llalinsky@live365.com>
2025-03-11 08:51:34 +0100

    More changes

--- acoustid/data/foreignid.py
+++ acoustid/data/foreignid.py
@@ -11,29 +11,31 @@ from acoustid.db import FingerprintDB
 logger = logging.getLogger(__name__)
 
 
-def get_foreignid(conn, id):
-    # type: (FingerprintDB, int) -> str
+def get_foreignid(conn: FingerprintDB, id: int) -> str | None:
     src = schema.foreignid.join(
         schema.foreignid_vendor,
         schema.foreignid.c.vendor_id == schema.foreignid_vendor.c.id,
     )
-    query = sql.select(
-        [
+    query = (
+        sql.select(
             schema.foreignid_vendor.c.name.label("namespace"),
             schema.foreignid.c.name.label("id"),
-        ],
-        schema.foreignid.c.id == id,
-        from_obj=src,
+        )
+        .select_from(src)
+        .where(schema.foreignid.c.id == id)
     )
+
     row = conn.execute(query).first()
-    return row["namespace"] + ":" + row["id"]
+    if row is None:
+        return None
+    return row.namespace + ":" + row.id
 
 
-def find_or_insert_foreignid_vendor(conn, name):
-    # type: (FingerprintDB, str) -> int
-    query = sql.select(
-        [schema.foreignid_vendor.c.id], schema.foreignid_vendor.c.name == name
+def find_or_insert_foreignid_vendor(conn: FingerprintDB, name: str) -> int:
+    query = sql.select(schema.foreignid_vendor.c.id).where(
+        schema.foreignid_vendor.c.name == name
     )
+
     id = conn.execute(query).scalar()
     if id is None:
         insert_stmt = schema.foreignid_vendor.insert().values(name=name)
@@ -42,16 +44,16 @@ def find_or_insert_foreignid_vendor(conn, name):
     return id
 
 
-def find_or_insert_foreignid(conn, full_name):
-    # type: (FingerprintDB, str) -> int
+def find_or_insert_foreignid(conn: FingerprintDB, full_name: str) -> int:
     vendor, name = full_name.split(":", 1)
     vendor_id = find_or_insert_foreignid_vendor(conn, vendor)
-    query = sql.select(
-        [schema.foreignid.c.id],
+    query = sql.select(schema.foreignid.c.id).where(
         sql.and_(
-            schema.foreignid.c.vendor_id == vendor_id, schema.foreignid.c.name == name
-        ),
+            schema.foreignid.c.vendor_id == vendor_id,
+            schema.foreignid.c.name == name,
+        )
     )
+
     id = conn.execute(query).scalar()
     if id is None:
         insert_stmt = schema.foreignid.insert().values(vendor_id=vendor_id, name=name)
--- acoustid/data/format.py
+++ acoustid/data/format.py
@@ -11,12 +11,11 @@ from acoustid.db import AppDB
 logger = logging.getLogger(__name__)
 
 
-def find_or_insert_format(conn, name):
-    # type: (AppDB, str) -> int
+def find_or_insert_format(conn: AppDB, name: str) -> int:
     """
     Find a format in the database, create it if it doesn't exist yet.
     """
-    query = sql.select([schema.format.c.id], schema.format.c.name == name)
+    query = sql.select(schema.format.c.id).where(schema.format.c.name == name)
     id = conn.execute(query).scalar()
     if id is None:
         insert_stmt = schema.format.insert().values(name=name)
