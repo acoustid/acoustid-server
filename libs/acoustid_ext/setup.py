ab10dd7da3b16f7faed8102a692e9177aa3118e4
Lukas Lalinsky <llalinsky@live365.com>
2025-03-11 16:33:01 +0100

    Add support for building Cython code

---
+++ libs/acoustid_ext/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        "acoustid_ext.fingerprint",
+        sources=["src/acoustid_ext/fingerprint.pyx"],
+        extra_compile_args=["-O3"]
+    )
+]
+
+setup(
+    name="acoustid_ext",
+    version="0.1.0",
+    packages=["acoustid_ext"],
+    package_dir={"": "src"},
+    ext_modules=cythonize(extensions, language_level="3"),
+)
---
+++ libs/acoustid_ext/src/acoustid_ext/fingerprint.pyx
@@ -0,0 +1,2 @@
+def add_numbers(int a, int b):
+    return a + b
