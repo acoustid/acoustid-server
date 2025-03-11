d8074bd608bbacbaf33540c1a0fe8dffd9c37eae
Lukas Lalinsky <llalinsky@live365.com>
2025-03-11 08:23:25 +0100

    More changes

--- acoustid/data/source.py
+++ acoustid/data/source.py
@@ -2,7 +2,7 @@
 # Distributed under the MIT license, see the LICENSE file for details.
 
 import logging
-from typing import Any, Dict, Optional
+from typing import Any
 
 from sqlalchemy import sql
 
@@ -12,31 +12,31 @@ from acoustid.db import AppDB
 logger = logging.getLogger(__name__)
 
 
-def get_source(conn, id):
-    # type: (AppDB, int) -> Optional[Dict[Any, Any]]
+def get_source(conn: AppDB, id: int) -> dict[str, Any] | None:
     query = sql.select(
-        [
-            schema.source.c.account_id,
-            schema.source.c.application_id,
-            schema.source.c.version,
-        ],
-        schema.source.c.id == id,
-    )
-    return conn.execute(query).first()
+        schema.source.c.account_id,
+        schema.source.c.application_id,
+        schema.source.c.version,
+    ).where(schema.source.c.id == id)
+    source = conn.execute(query).first()
+    if source is None:
+        return None
+    # TODO return Row
+    return dict(source._mapping)
 
 
-def find_or_insert_source(conn, application_id, account_id, version=None):
-    # type: (AppDB, int, int, Optional[str]) -> int
+def find_or_insert_source(
+    conn: AppDB, application_id: int, account_id: int, version: str | None = None
+) -> int:
     """
     Find a source in the database, create it if it doesn't exist yet.
     """
-    query = sql.select(
-        [schema.source.c.id],
+    query = sql.select(schema.source.c.id).where(
         sql.and_(
             schema.source.c.account_id == account_id,
             schema.source.c.application_id == application_id,
             schema.source.c.version == version,
-        ),
+        )
     )
     id = conn.execute(query).scalar()
     if id is None:
