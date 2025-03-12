9c08bd2d2995ab97676ddf43bea902ae963bfbe9
Lukas Lalinsky <llalinsky@live365.com>
2025-03-12 22:45:09 +0100

    More fixes

--- tests/test_script_backfill_meta_gid.py
+++ tests/test_script_backfill_meta_gid.py
@@ -22,8 +22,7 @@ from . import (
 
 
 @with_script_context
-def test_get_update_last_meta_id(ctx):
-    # type: (ScriptContext) -> None
+def test_get_update_last_meta_id(ctx: ScriptContext) -> None:
     fingerprint_db = ctx.db.get_fingerprint_db()
     assert 0 == get_last_meta_id(fingerprint_db)
     assert 0 == get_last_meta_id(fingerprint_db)
@@ -34,7 +33,7 @@ def test_get_update_last_meta_id(ctx):
 
 
 @with_script_context
-def test_backfill_meta_gid_update(ctx):
+def test_backfill_meta_gid_update(ctx: ScriptContext) -> None:
     app_db = ctx.db.get_app_db()
     fingerprint_db = ctx.db.get_fingerprint_db()
     ingest_db = ctx.db.get_ingest_db()
@@ -49,49 +48,49 @@ def test_backfill_meta_gid_update(ctx):
             "meta": {"track": "Foo"},
         },
     )
-    query = tables.submission.select(tables.submission.c.id == submission_id)
-    submission = ingest_db.execute(query).fetchone()
+    query = tables.submission.select().where(tables.submission.c.id == submission_id)
+    submission = ingest_db.execute(query).one()
     assert submission is not None
 
     fingerprint = import_submission(
-        ingest_db, app_db, fingerprint_db, ctx.index, submission
+        ingest_db, app_db, fingerprint_db, ctx.index, submission._mapping
     )
     assert fingerprint is not None
 
-    query = tables.track_meta.select(
+    query = tables.track_meta.select().where(
         tables.track_meta.c.track_id == fingerprint["track_id"]
     )
-    track_meta = fingerprint_db.execute(query).fetchone()
-    assert 1 == track_meta["submission_count"]
+    track_meta = fingerprint_db.execute(query).one()
+    assert 1 == track_meta.submission_count
 
-    query = (
+    update_stmt = (
         tables.meta.update()
         .values({"gid": None})
-        .where(tables.meta.c.id == track_meta["meta_id"])
+        .where(tables.meta.c.id == track_meta.meta_id)
     )
-    fingerprint_db.execute(query)
+    fingerprint_db.execute(update_stmt)
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert meta["gid"] is None
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert meta.gid is None
 
-    query = (
+    update_stmt = (
         tables.submission_result.update()
         .values({"meta_gid": None})
-        .where(tables.submission_result.c.meta_id == track_meta["meta_id"])
+        .where(tables.submission_result.c.meta_id == track_meta.meta_id)
     )
-    ingest_db.execute(query)
+    ingest_db.execute(update_stmt)
 
     last_meta_id = backfill_meta_gid(fingerprint_db, ingest_db, 0, 100)
     assert 3 == last_meta_id
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta["gid"]
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta.gid
 
 
 @with_script_context
-def test_backfill_meta_gid_merge(ctx):
+def test_backfill_meta_gid_merge(ctx: ScriptContext) -> None:
     app_db = ctx.db.get_app_db()
     fingerprint_db = ctx.db.get_fingerprint_db()
     ingest_db = ctx.db.get_ingest_db()
@@ -106,36 +105,36 @@ def test_backfill_meta_gid_merge(ctx):
             "meta": {"track": "Foo"},
         },
     )
-    query = tables.submission.select(tables.submission.c.id == submission_id)
-    submission = ingest_db.execute(query).fetchone()
+    query = tables.submission.select().where(tables.submission.c.id == submission_id)
+    submission = ingest_db.execute(query).one()
     assert submission is not None
 
     fingerprint = import_submission(
-        ingest_db, app_db, fingerprint_db, ctx.index, submission
+        ingest_db, app_db, fingerprint_db, ctx.index, submission._mapping
     )
     assert fingerprint is not None
 
-    query = tables.track_meta.select(
+    query = tables.track_meta.select().where(
         tables.track_meta.c.track_id == fingerprint["track_id"]
     )
-    track_meta = fingerprint_db.execute(query).fetchone()
-    assert 1 == track_meta["submission_count"]
-    assert 3 == track_meta["meta_id"]
+    track_meta = fingerprint_db.execute(query).one()
+    assert 1 == track_meta.submission_count
+    assert 3 == track_meta.meta_id
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta["gid"]
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta.gid
 
-    query = (
+    update_stmt = (
         tables.meta.update()
         .values({"gid": None})
-        .where(tables.meta.c.id == track_meta["meta_id"])
+        .where(tables.meta.c.id == track_meta.meta_id)
     )
-    fingerprint_db.execute(query)
+    fingerprint_db.execute(update_stmt)
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert meta["gid"] is None
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert meta.gid is None
 
     submission_id = insert_submission(
         ingest_db,
@@ -147,31 +146,31 @@ def test_backfill_meta_gid_merge(ctx):
             "meta": {"track": "Foo"},
         },
     )
-    query = tables.submission.select(tables.submission.c.id == submission_id)
-    submission = ingest_db.execute(query).fetchone()
+    query = tables.submission.select().where(tables.submission.c.id == submission_id)
+    submission = ingest_db.execute(query).one()
     assert submission is not None
 
     fingerprint = import_submission(
-        ingest_db, app_db, fingerprint_db, ctx.index, submission
+        ingest_db, app_db, fingerprint_db, ctx.index, submission._mapping
     )
     assert fingerprint is not None
 
     last_meta_id = backfill_meta_gid(fingerprint_db, ingest_db, 0, 100)
     assert 4 == last_meta_id
 
-    query = tables.track_meta.select(
+    query = tables.track_meta.select().where(
         tables.track_meta.c.track_id == fingerprint["track_id"]
     )
-    track_meta = fingerprint_db.execute(query).fetchone()
-    assert 4 == track_meta["meta_id"]
+    track_meta = fingerprint_db.execute(query).one()
+    assert 4 == track_meta.meta_id
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta["gid"]
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta.gid
 
 
 @with_script_context
-def test_backfill_meta_gid_merge_duplicate(ctx):
+def test_backfill_meta_gid_merge_duplicate(ctx: ScriptContext) -> None:
     app_db = ctx.db.get_app_db()
     fingerprint_db = ctx.db.get_fingerprint_db()
     ingest_db = ctx.db.get_ingest_db()
@@ -186,36 +185,36 @@ def test_backfill_meta_gid_merge_duplicate(ctx):
             "meta": {"track": "Foo"},
         },
     )
-    query = tables.submission.select(tables.submission.c.id == submission_id)
-    submission = ingest_db.execute(query).fetchone()
+    query = tables.submission.select().where(tables.submission.c.id == submission_id)
+    submission = ingest_db.execute(query).one()
     assert submission is not None
 
     fingerprint = import_submission(
-        ingest_db, app_db, fingerprint_db, ctx.index, submission
+        ingest_db, app_db, fingerprint_db, ctx.index, submission._mapping
     )
     assert fingerprint is not None
 
-    query = tables.track_meta.select(
+    query = tables.track_meta.select().where(
         tables.track_meta.c.track_id == fingerprint["track_id"]
     )
-    track_meta = fingerprint_db.execute(query).fetchone()
-    assert 1 == track_meta["submission_count"]
-    assert 3 == track_meta["meta_id"]
+    track_meta = fingerprint_db.execute(query).one()
+    assert 1 == track_meta.submission_count
+    assert 3 == track_meta.meta_id
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta["gid"]
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta.gid
 
-    query = (
+    update_stmt = (
         tables.meta.update()
         .values({"gid": None})
-        .where(tables.meta.c.id == track_meta["meta_id"])
+        .where(tables.meta.c.id == track_meta.meta_id)
     )
-    fingerprint_db.execute(query)
+    fingerprint_db.execute(update_stmt)
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert meta["gid"] is None
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert meta.gid is None
 
     submission_id = insert_submission(
         ingest_db,
@@ -227,24 +226,24 @@ def test_backfill_meta_gid_merge_duplicate(ctx):
             "meta": {"track": "Foo"},
         },
     )
-    query = tables.submission.select(tables.submission.c.id == submission_id)
-    submission = ingest_db.execute(query).fetchone()
+    query = tables.submission.select().where(tables.submission.c.id == submission_id)
+    submission = ingest_db.execute(query).one()
     assert submission is not None
 
     fingerprint = import_submission(
-        ingest_db, app_db, fingerprint_db, ctx.index, submission
+        ingest_db, app_db, fingerprint_db, ctx.index, submission._mapping
     )
     assert fingerprint is not None
 
     last_meta_id = backfill_meta_gid(fingerprint_db, ingest_db, 0, 100)
     assert 4 == last_meta_id
 
-    query = tables.track_meta.select(
+    query = tables.track_meta.select().where(
         tables.track_meta.c.track_id == fingerprint["track_id"]
     )
-    track_meta = fingerprint_db.execute(query).fetchone()
-    assert 4 == track_meta["meta_id"]
+    track_meta = fingerprint_db.execute(query).one()
+    assert 4 == track_meta.meta_id
 
-    query = tables.meta.select().where(tables.meta.c.id == track_meta["meta_id"])
-    meta = fingerprint_db.execute(query).fetchone()
-    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta["gid"]
+    query = tables.meta.select().where(tables.meta.c.id == track_meta.meta_id)
+    meta = fingerprint_db.execute(query).one()
+    assert uuid.UUID("da570fc1-ecfd-5fcd-86d7-009daa0f79e5") == meta.gid
