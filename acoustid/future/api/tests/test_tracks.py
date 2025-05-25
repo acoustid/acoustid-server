6f2910e515b86a49d8285d609a66e2eff8ad9adf
Lukas Lalinsky <llalinsky@live365.com>
2025-05-25 20:25:00 +0200

    More tests

--- acoustid/future/api/tests/test_tracks.py
+++ acoustid/future/api/tests/test_tracks.py
@@ -6,7 +6,24 @@ def test_list_by_mbid(client: TestClient) -> None:
         "/v3/track/list_by_mbid",
         params={"mbid": "38f33829-0a01-4593-b000-c8900902d1e8"},
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.text
+    assert response.json() == {"tracks": []}
+
+
+def test_list_by_mbid_missing_mbid(client: TestClient) -> None:
+    response = client.get(
+        "/v3/track/list_by_mbid",
+        params={},
+    )
+    assert response.status_code == 400, response.text
+
+
+def test_list_by_mbid_invalid_mbid(client: TestClient) -> None:
+    response = client.get(
+        "/v3/track/list_by_mbid",
+        params={"mbid": "abc"},
+    )
+    assert response.status_code == 400, response.text
 
 
 def test_list_by_fingerprint(client: TestClient) -> None:
@@ -15,3 +32,20 @@ def test_list_by_fingerprint(client: TestClient) -> None:
         params={"id": "1"},
     )
     assert response.status_code == 200, response.text
+    assert response.json() == {"tracks": []}
+
+
+def test_list_by_fingerprint_missing_id(client: TestClient) -> None:
+    response = client.get(
+        "/v3/track/list_by_fingerprint",
+        params={},
+    )
+    assert response.status_code == 400, response.text
+
+
+def test_list_by_fingerprint_invalid_id(client: TestClient) -> None:
+    response = client.get(
+        "/v3/track/list_by_fingerprint",
+        params={"id": "abc"},
+    )
+    assert response.status_code == 400, response.text
